c5404ed11e8aed09c557bba59a80b00edc0856f6
Ismai-ts <tsurovismail37@gmail.com>
2024-08-12 06:08:24 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,75 +1,143 @@
-const screens= document.querySelectorAll('.screen');
-const chooseSweetBtns = document.querySelectorAll('.choose_sweet_btn');
-const statr_btn= document.getElementById('start_btn');
-const gameNode= document.getElementById('game_container');
-const timeEl= document.getElementById('time');
-const scoreEl= document.getElementById('score');
+const GAME_NODE = document.querySelector("#game-board");
+const WINNING_TEXT = document.querySelector("#victory-message");
+const START_GAME_BUTTON = document.querySelector("#new-game-button");
 
-let seconds=0;
-let score=0;
-let selectedSweet={};
+const VISIBLE_CARD_CLASSNAME = "visible";
+const CARD_FLIP_TIMEOUT_MS = 500;
 
-statr_btn.addEventListener('click',()=>{
-    screens[0].classList.remove('visible');
-    screens[1].classList.add('visible');
-})
+const CARD_ELEMENTS = ["🍓", "🍉", "🍌", "🍏", "🥝", "🍇"];
+const CARDS_AMOUNT = 12;
 
-chooseSweetBtns.forEach((btn) =>{
-    btn.addEventListener('click',() =>{
-        const img = btn.querySelector('img');
-        const src = img.getAttribute('src');
-        selectedSweet = { src };
-        screens[1].classList.remove('visible');
-        screens[2].classList.add('visible');
-        startGame();
-    })
-})
+let VISIBLE_CARDS = [];
 
-function startGame(){
-    setInterval(increaseTime, 1000);
-    createSweet();
-}
-function increaseTime(){
-    timeEl.innerHTML= `Время: ${seconds}`;
-    seconds++;
-}
-function createSweet(){
- const{x,y}= getRandomLocation();
+START_GAME_BUTTON.addEventListener("click", startGame);
 
- const sweet= document.createElement('img');
- sweet.src= selectedSweet.src;
- sweet.classList.add('sweet');
- sweet.style.display= 'block';
- sweet.style.top=`${y}px`;
- sweet.style.left=`${x}px`;
- sweet.style.transform=`rotate(${Math.random()*360})deg`;
- sweet.addEventListener('click',catchSweet)
- gameNode.appendChild(sweet);
-}
-function getRandomLocation(){
-    const width = window.innerWidth;
-    const height = window.innerHeight;
+function startGame() {
+  // Очищаем игровое поле
+  [GAME_NODE, WINNING_TEXT].forEach((element) => (element.innerHTML = ""));
 
-    const x = (Math.random() * (width-100));
-    const y = (Math.random() * (height-100));
-    return{x,y};
-}
-function playBiteSound(){
-    const audio= document.getElementById('audio');
-    audio.play();
-}
-function catchSweet(){
-    playBiteSound();
-    increaseScore();
+  const CARD_VALUES = generateArrayWithPairs(CARD_ELEMENTS, CARDS_AMOUNT);
 
-    this.remove();
-    addSweet();
+  CARD_VALUES.forEach(renderCard);
 
+  const renderedCards = document.querySelectorAll(".card");
+
+  renderedCards.forEach((card) => card.classList.add(VISIBLE_CARD_CLASSNAME));
+
+  setTimeout(() => {
+    renderedCards.forEach((card) =>
+      card.classList.remove(VISIBLE_CARD_CLASSNAME)
+    );
+  }, CARD_FLIP_TIMEOUT_MS * 2);
 }
-function addSweet(){
-    setTimeout(createSweet, 1000);
+
+function generateArrayWithPairs(arr, fieldSize) {
+  if (arr.length * 2 !== fieldSize) {
+    const errorMessage =
+      "Невозможно создать массив с парами из указанного массива и размера.";
+
+    console.error(errorMessage);
+    return null;
+  }
+
+  const randomArray = [];
+  const elementCounts = {};
+
+  for (const item of arr) {
+    elementCounts[item] = 0;
+  }
+
+  while (randomArray.length < fieldSize) {
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    const randomElement = arr[randomIndex];
+
+    if (elementCounts[randomElement] < 2) {
+      randomArray.push(randomElement);
+      elementCounts[randomElement]++;
+    }
+  }
+
+  return randomArray;
 }
-function increaseScore(){
-    score++;
-    scoreEl.innerHTML=`Счет: ${score}`;
+
+function renderCard(cardText = "") {
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  const cardInner = document.createElement("div");
+  cardInner.classList.add("card-inner");
+
+  const cardFront = document.createElement("div");
+  cardFront.classList.add("card-front");
+
+  const cardBack = document.createElement("div");
+  cardBack.classList.add("card-back");
+
+  cardFront.textContent = "?";
+  cardBack.textContent = cardText;
+
+  cardInner.appendChild(cardFront);
+  cardInner.appendChild(cardBack);
+
+  card.appendChild(cardInner);
+
+  card.addEventListener("click", handleCardClick.bind(this, card));
+
+  GAME_NODE.appendChild(card);
 }
+
+function handleCardClick(card) {
+  // Не реагируем на нажатие на открытую карточку
+  if (card.classList.contains(VISIBLE_CARD_CLASSNAME)) {
+    return;
+  }
+
+  // Функция проверки выигрыша
+  const checkVictory = () => {
+    const visibleCardsNodes = document.querySelectorAll(
+      `.${VISIBLE_CARD_CLASSNAME}`
+    );
+
+    // Если кол-во открытых карте равно общему кол-ву карт, то это победа
+
+    const isVictory = visibleCardsNodes.length === CARDS_AMOUNT;
+    const victoryMessage = "Поздравляю, вы победили!";
+
+    if (isVictory) {
+      WINNING_TEXT.textContent = victoryMessage;
+    }
+  };
+
+  // Проверяем на выигрыш после анимации открытия карты
+  card
+    .querySelector(".card-inner")
+    .addEventListener("transitionend", checkVictory);
+
+  // Добавляем карте класс visible, запуская анимацию поворота
+  card.classList.add(VISIBLE_CARD_CLASSNAME);
+
+  // Добавляем карту в массив открытых карт
+  VISIBLE_CARDS.push(card);
+
+  // Так как нам нужно проверять каждые 2 отрытые карты, делаем такое условие
+  if (VISIBLE_CARDS.length % 2 !== 0) {
+    return;
+  }
+
+  // Получаем последнюю и предпоследнюю открытые карты, чтобы проверять совпадают ли они
+  const [prelastCard, lastCard] = VISIBLE_CARDS.slice(-2);
+
+  // Если две последние открытые карты не совпадают, убираем их из массива открытых карт
+  if (lastCard.textContent !== prelastCard.textContent) {
+    VISIBLE_CARDS = VISIBLE_CARDS.slice(0, VISIBLE_CARDS.length - 2);
+
+    // Через 500 мс закрываем те карты, которые не совпали
+    setTimeout(() => {
+      [lastCard, prelastCard].forEach((card) =>
+        card.classList.remove(VISIBLE_CARD_CLASSNAME)
+      );
+    }, CARD_FLIP_TIMEOUT_MS);
+  }
+}
+
+startGame();
